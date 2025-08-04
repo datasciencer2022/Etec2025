0f91c7412cee25b872a3116e44b9aa1be7d39c39
GestaoPsi <103309402+datasciencer2022@users.noreply.github.com>
2025-08-04 14:21:14 -0300

    Update Senha.java

--- Etec2025/senha/Senha.java
+++ Etec2025/senha/Senha.java
@@ -1,1 +1,44 @@
+package senha;
+public class Senha {
+	private int tamanho;
+	private String senha;
+	
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public Character[] gerarMaiusculas() {
+		Character[] maiusculas = new Character[26];
+		for (int i = 0; i < 26; i++) {
+		    maiusculas[i] = (char) ('A' + i);
+		}
+		return maiusculas;
+	}
+	
+	public Character[] gerarMinusculas() {
+		Character[] minusculas =  new Character[26];
+		for (int i = 0; i < 26; i++) {
+		    minusculas[i] = (char) ('a' + i);
+		}
+		return minusculas;
+	}
+	
+	public Character[] gerarNumeros() {
+		Character[] nums = {'0','1','2','3','4',
+				'5','6','7','8','9'};
+		return nums;
+	}
+}
 
